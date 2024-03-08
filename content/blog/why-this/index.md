f2e50a28b18174fb4cee8e28c73ffeab6f6abd1a
Maitra Khatri <maitrakhatri@gmail.com>
2024-03-08 12:20:38 +0530

    Set-up and First Blog

---
+++ content/blog/why-this/index.md
@@ -0,0 +1,24 @@
+---
+title: Why this ?
+date: "2015-05-01T22:12:03.284Z"
+description: "Why a React Framework and not any other easy to use blogging platform ?"
+---
+
+Why a React Framework and not any other easy to use blogging platform ?
+
+Firstly, I know nothing about this, not a single thing. I just forked a boilerplate and started writing this. Because is the motive of this entire thing is that I don't wanna care about anything and just focus on writing.
+
+Then you might ask why this fancy thing Maitra ?
+
+1. To flex that you're a developer ?
+2. To just increase the green dots on your GitHub ?
+3. Are you too posh to use blogger or notion or anything else like them ?
+
+Umm, no. I do have a blog on Notion, it's just too goodd !!! But the biggest problem with it is
+
+# S E O
+
+Yes, there is ZERO SEO on it. I personally love writing stuff, I don't expect to earn money from it but I do want people to read it. If someone googles my name then I want it to show up.
+React apps in general have good seo without doing seo, my random test projects show up in google search.
+
+Now you're next question might be then why not NEXT JS ? It's trending, it has strong future and crazy strong seo. One word answer - **laziness** . NextJS is good but its a bit complex and my motive is to right now focus on writing only.
--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Maitra Khatri Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Maitra Khatri`,
+      summary: `who lives and works in Ahmedabad writing interesting things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `Maitra Khatri writes stuff`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `maitrakhatri`,
     },
   },
   plugins: [
@@ -110,15 +110,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `Maitra Khatri blog`,
+        short_name: `Maitra Khatri`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/profile-pic.png`, // This path is relative to the root of the site.
       },
     },
   ],
